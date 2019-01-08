ae5b9f06bac3801b215d53d8f4b4ec58fe8eaf68
Bryan Leighton <bryan.leigh77@gmail.com>
2019-01-08 13:52:57 +0100

    fix contained variant of button from material UI

--- src/components/content/tabs/tab/Tab.js
+++ src/components/content/tabs/tab/Tab.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
 class Tab extends Component {
-		
+
 		handleClick = () => {
 		if(this.props.setActive){
 			const name = this.props.name;
 			this.props.setActive(name)
 		}
 	}
-		 
+
 	render() {
 		const classes = 'info-tab '+ (this.props.name === this.props.active ? "active-tab" : "" );
 		const contained = this.props.active === this.props.name ? "outlined" : "";
 		return (
-			<Button color="default" onClick={this.handleClick} className={classes} variant={contained} >
+			<Button color="default" onClick={this.handleClick} className={classes} >
 					{this.props.name}
 			</Button>
 		);
